f830873d0076a51b5ed9804c5bbdc2ecb59ef052
nurzainpradana <pradananurzain@gmail.com>
2020-10-25 05:43:49 +0700

    materi 6. Initializer Block

--- src/main/kotlin/data/Car.kt
+++ src/main/kotlin/data/Car.kt
@@ -1,6 +1,12 @@
 package data
 
 class Car(paramBrand: String, paramYear: Int = 2020) {
+
+    // println("Car Dibuat") //ERROR
+    init {
+        println("Car $paramBrand dibuat")
+    }
+
     var brand: String = paramBrand
     var year: Int = paramYear
 }
