a8256b00ec68d4dbe40ca2fdebe8102e6bb6393e
nurzainpradana <pradananurzain@gmail.com>
2020-10-25 12:33:19 +0700

    materi 13. Function Overriding

--- src/main/kotlin/data/Employee.kt
+++ src/main/kotlin/data/Employee.kt
@@ -1,11 +1,28 @@
 package data
 
 open class Employee(val name: String) {
-    fun sayHello(name: String){
+    open fun sayHello(name: String){
         println("Hello, my name is ${this.name}")
     }
 }
 
-class Manager(name: String): Employee(name)
+open class Manager(name: String): Employee(name) {
+    final override fun sayHello(name: String) {
+        println("Hello, my name is Manager ${this.name}")
+    }
+}
 
-class VicePresident(name: String): Employee(name)
+class SuperManager(name: String) : Manager(name) {
+    /* override fun sayHello(name: String) {
+        println("Hello, my name is Super Manager ${this.name}")
+    }
+
+    ERROR Karena function pada manager bersifat final
+     */
+}
+
+class VicePresident(name: String): Employee(name) {
+    override fun sayHello(name: String) {
+        println("Hello, my name is Vice Manager ${this.name}")
+    }
+}
