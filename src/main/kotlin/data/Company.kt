4669e5ecd1e352094e90b2828fe9174265eb0aec
nurzainpradana <pradananurzain@gmail.com>
2020-10-25 20:20:55 +0700

    materi 21. Hash Code Function

--- src/main/kotlin/data/Company.kt
+++ src/main/kotlin/data/Company.kt
@@ -1,6 +1,10 @@
 package data
 
 class Company(val name: String) {
+    override fun hashCode(): Int {
+        return name.hashCode()
+    }
+
     override fun equals(other: Any?): Boolean {
         return when (other) {
             is Company -> other.name == this.name
